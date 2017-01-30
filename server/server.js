10e607ed7cb874b63058d089829002cc37a544b2
abhex <abdul.basit.hex@gmail.com>
2017-01-30 11:30:55 +0500

    Part-3 Done

--- server/server.js
+++ server/server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+var config = require('../webpack.config');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
 
+var compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
 app.use(express.static('./dist'));
 
 app.use('/', function (req, res) {
